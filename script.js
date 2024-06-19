3dbdca0162a550dc87595fab33fc580e9b68ab97
Jesse Smart <jsmartdev@protonmail.com>
2024-06-19 10:26:16 -0400

    display pokemon name working

--- script.js
+++ script.js
@@ -20,11 +20,31 @@ const getPokemon = () => {
   fetch(pokemonUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      displayPokemon(data);
     })
     .catch(err => {
       console.error('Error fetching current weather data:', err)
       alert('Error fetching current weather data! Please try again!')
     });
 
+}
+
+const displayPokemon = (data) => {
+
+  const pokemonInfo = document.getElementById('pokemon-info');
+  const pokemonSprite = document.getElementById('pokemon-sprite');
+
+  clearDiv(pokemonInfo);
+  clearDiv(pokemonSprite);
+
+  if (data.cod === '404') {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = data.message;
+    pokemonInfo.appendChild(paragraph);
+  } else {
+    const pokemonName = data.name;
+    const nameParagraph =  document.createElement('p');
+    nameParagraph.textContent = `${pokemonName}`;
+    pokemonInfo.appendChild(nameParagraph);
+  }
 }
