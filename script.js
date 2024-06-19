d582efd091f6a864e331ca1c06443d853402ec0d
Jesse Smart <jsmartdev@protonmail.com>
2024-06-19 15:39:41 -0400

    getting names of all pokemon displaying

--- script.js
+++ script.js
@@ -1,36 +1,26 @@
-
-const clearDiv = (div) => {
-  while(div.firstChild) {
-    div.removeChild(div.firstChild)
-  }
-}
+let allPokemon = [];
 
 const getPokemon = () => {
-
-  const pokemon = document.getElementById('pokemon-input');
-  const pokemonValue = pokemon.value;
-
-  if(!pokemonValue) {
-    alert('Please enter a pokemon!');
-    return;
-  }
   
-  const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.value}/`
-
-  fetch(pokemonUrl)
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
     .then(response => response.json())
     .then(data => {
-      displayPokemon(data);
-      console.log(data);
-    })
-    .catch(err => {
-      console.error('Error fetching pokemon:', err)
-      alert('Error fetching pokemon! Please try again!')
+      const allPokemon = data.results;
+      console.log(allPokemon)
+      allPokemon.forEach(element => {
+        const paragraph = document.createElement('p');
+        const pokemonInfo = document.getElementById('pokemon-info');
+        paragraph.textContent = `Name: ${element.name}`;
+        paragraph.classList.add('poke-card')
+        pokemonInfo.appendChild(paragraph);
+      });
     });
-
 }
 
-const displayPokemon = (data) => {
+getPokemon(allPokemon);
+
+
+/*const displayPokemon = (data) => {
 
   const pokemonInfo = document.getElementById('pokemon-info');
   const pokemonSprite = document.getElementById('pokemon-sprite');
@@ -44,7 +34,7 @@ const displayPokemon = (data) => {
     pokemonInfo.appendChild(paragraph);
   } else {
     const id = data.id;
-    const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+    const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
     const pokemonName = data.name;
     const pokemonWeight = data.weight;
     const nameParagraph =  document.createElement('p');
@@ -55,4 +45,14 @@ const displayPokemon = (data) => {
     pokemonInfo.appendChild(weightParagraph);
     pokemonSprite.src = spriteUrl;
   }
-}
+}*/
+
+/*pokemon.forEach(element => {
+  const name = element.name;
+  const id = element.id;
+  const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+  const nameParagraph =  document.createElement('p');
+  nameParagraph.textContent = `${name}`;
+  pokemonInfo.appendChild(nameParagraph);
+  pokemonSprite.src = imageUrl;
+});*/
