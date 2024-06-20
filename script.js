c129e6fee49bb9de5be21ea95ee91ce5aa9ffd39
Jesse Smart <jsmartdev@protonmail.com>
2024-06-20 16:13:19 -0400

    display pokemon artwork functional

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ const getPokemon = () => {
       const allPokemon = data.results;
       console.log(allPokemon)
       allPokemon.forEach(element => {
+        const card = document.createElement('div');
         const paragraph = document.createElement('p');
+        const pokemonImage = document.createElement('img');
         const pokemonInfo = document.getElementById('pokemon-info');
+        const id = element.url.split('/')[6];
+        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
         paragraph.textContent = `Name: ${element.name}`;
-        paragraph.classList.add('poke-card')
-        pokemonInfo.appendChild(paragraph);
+        card.classList.add('poke-card');
+        pokemonImage.classList.add('poke-image');
+        pokemonImage.src = imageUrl;
+        card.appendChild(pokemonImage);
+        card.appendChild(paragraph);
+        pokemonInfo.appendChild(card);
       });
     });
 }
