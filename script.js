435d7196d41dd57384f09ed112b79a847d414d8d
Jesse Smart <jsmartdev@protonmail.com>
2024-06-21 16:51:32 -0400

    changed getPokemon function to async await

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 let allPokemon = [];
 
-const getPokemon = () => {
+const getPokemon = async () => {
   
-  fetch(`https://pokeapi.co/api/v2/pokemon?limit=200`)
-    .then(response => response.json())
-    .then(data => {
-      const allPokemon = data.results;
-      console.log(allPokemon)
-      allPokemon.forEach(element => {
-        const card = document.createElement('div');
-        const paragraph = document.createElement('p');
-        const pokemonImage = document.createElement('img');
-        const pokemonInfo = document.getElementById('pokemon-info');
-        const id = element.url.split('/')[6];
-        const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
-        paragraph.textContent = `Name: ${element.name}`;
-        card.classList.add('poke-card');
-        pokemonImage.classList.add('poke-image');
-        pokemonImage.src = imageUrl;
-        card.appendChild(pokemonImage);
-        card.appendChild(paragraph);
-        pokemonInfo.appendChild(card);
-      });
-    });
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=1025`;
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch(err) {
+    console.error(err);
+  }
 }
+
+getPokemon().then(data => {
+  
+  const allPokemon = data.results;
+  
+  allPokemon.forEach(element => {
+    const card = document.createElement('div');
+    const paragraph = document.createElement('p');
+    const pokemonImage = document.createElement('img');
+    const pokemonInfo = document.getElementById('pokemon-info');
+    const id = element.url.split('/')[6];
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+    paragraph.textContent = `Name: ${element.name}`;
+    card.classList.add('poke-card');
+    pokemonImage.classList.add('poke-image');
+    pokemonImage.src = imageUrl;
+    card.appendChild(pokemonImage);
+    card.appendChild(paragraph);
+    pokemonInfo.appendChild(card);
+  })
+
+})
 
 getPokemon(allPokemon);
 
